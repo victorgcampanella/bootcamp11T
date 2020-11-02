fd46052454d345a3bbda12d6a9029e5d73168e23
victorgcampanella <vgcgalvao@outlook.com>
2020-11-02 15:20:25 -0300

    tipando objetos e vetores

--- typescript/dist/routes.js
+++ typescript/dist/routes.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helloWorld(request, response) {
-    return response.json({ message: 'Hello World' });
+    var user = CreateUser_1.default({
+        name: 'Victor',
+        email: 'victor@gmail.com',
+        password: '123456',
+        techs: [
+            'React',
+            'ReactNative',
+            { title: 'JavaScript', experience: 100 }
+        ]
+    });
+    return response.json(user);
 }
 exports.helloWorld = helloWorld;
--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,17 @@
 import {Request, Response} from 'express'
+import createUser from './services/CreateUser'
 
 export function helloWorld(request: Request, response: Response) {
-  return response.json({message: 'Hello World'})
+  const user = createUser({
+    name: 'Victor',
+    email: 'victor@gmail.com',
+    password: '123456',
+    techs: [
+      'React', 
+      'ReactNative',
+      {title: 'JavaScript', experience: 100}
+    ]
+  })
+
+  return response.json(user)
 }
