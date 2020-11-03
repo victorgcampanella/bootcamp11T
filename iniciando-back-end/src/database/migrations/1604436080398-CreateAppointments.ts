a10643941297c172929d1c83ec2517a74d0179ce
victorgcampanella <vgcgalvao@outlook.com>
2020-11-03 19:27:15 -0300

    reestruturando arquivos para agendamentos

--- iniciando-back-end/src/database/migrations/1604436080398-CreateAppointments.ts
+++ iniciando-back-end/src/database/migrations/1604436080398-CreateAppointments.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateAppointments1604436080398
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
@@ -12,6 +13,7 @@ export default class CreateAppointments1604436080398
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
