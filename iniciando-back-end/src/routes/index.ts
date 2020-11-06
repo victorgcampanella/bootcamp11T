a22195f2b0c6f78923a0dc51d280f7ff10398596
victorgcampanella <vgcgalvao@outlook.com>
2020-11-05 22:35:49 -0300

    criacao de registros de usuarios

--- iniciando-back-end/src/routes/index.ts
+++ iniciando-back-end/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appoitmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appoitmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
