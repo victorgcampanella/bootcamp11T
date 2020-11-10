275c39c7b008433b47c258f2cea4ce29014492d6
victorgcampanella <vgcgalvao@outlook.com>
2020-11-10 19:53:00 -0300

    validando credenciais

--- iniciando-back-end/src/routes/index.ts
+++ iniciando-back-end/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import sessionsRouter from './sessions.routes';
 import appoitmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
 
 const routes = Router();
 
+routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appoitmentsRouter);
 routes.use('/users', usersRouter);
 
