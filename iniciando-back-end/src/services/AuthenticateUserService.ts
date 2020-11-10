f63f15283ce0d36eec6cb1d4d33bede7bf2c8d88
victorgcampanella <vgcgalvao@outlook.com>
2020-11-10 20:10:38 -0300

    gerando token jwt

--- iniciando-back-end/src/services/AuthenticateUserService.ts
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface RequestDTO {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, 'e74bc45d06382a7863fd77af1da2965a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
