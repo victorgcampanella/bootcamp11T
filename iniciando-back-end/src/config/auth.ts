e81c5465583dc06b12e522231ce1533f7b35b498
victorgcampanella <vgcgalvao@outlook.com>
2020-11-10 21:08:12 -0300

    rotas autenticadas

---
+++ iniciando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e74bc45d06382a7863fd77af1da2965a',
+    expiresIn: '1d',
+  },
+};
--- iniciando-back-end/src/services/AuthenticateUserService.ts
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface RequestDTO {
@@ -30,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, 'e74bc45d06382a7863fd77af1da2965a', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
